37812f5b58c257139c47f8b10df73e5b4900ab4c
m00n-die <reginaldrock219@gmail.com>
2023-11-23 04:36:37 -0800

    added task 06

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -4,6 +4,8 @@ const app = express();
 const port = 1245;
 
 app.get('/', (request, response) => {
+  response.status(202);
+  response.setHeader('Content-Type', 'text/plain');
   response.send('Hello Holberton School!');
 });
 
