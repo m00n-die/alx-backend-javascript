59c063cf455acdb87c93c579a4d746a062251fc7
m00n-die <reginaldrock219@gmail.com>
2023-11-25 15:42:11 +0000

    added first files

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,8 @@
+const assert = requiire('assert.js');
+const calculateNumber = require("./0-calcul.js");
+
+describe('test suite 1', calculateNumber() {
+  it('adds numbers' , calculateNumber() {
+    assert.equal(calculateNumber(1, 3), 4);
+  });
+});
