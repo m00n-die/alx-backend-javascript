725bcee84f14c90cc945e47ecc6014a3d9cf93ff
m00n-die <reginaldrock219>
2023-11-25 16:40:24 +0000

    added task 02

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,20 +1,23 @@
 function calculateNumber(type, a, b) {
-    if (type === 'SUM') {
-      return Math.round(a) + Math.round(b);
-    }
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);  
 
-    if (type === 'SUBTRACT') {
-      return Math.round(a) - Math.round(b);
-    }
-
-    if (type === 'DIVIDE') {
-      if (b === 0) {
-        const msg = 'Error';
-        return msg;
-      }
-      return Math.round(a) / Math.round(b);
-    }
-    
+  if (type === 'SUM') {
+    return roundA + roundB;
   }
+
+  if (type === 'SUBTRACT') {
+    return roundA - roundB;
+  }
+
+  if (type === 'DIVIDE') {
+    if (b === 0) {
+      const msg = 'Error';
+      return msg;
+    }
+    return roundA / roundB;
+  }
+    
+}
   
 module.exports = calculateNumber;
