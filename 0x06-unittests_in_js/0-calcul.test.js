84219a0f87e6c5e5b7a1700cdb5d90a50f40cbf8
m00n-die <reginaldrock219>
2023-11-25 16:04:50 +0000

    fixed

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,8 +1,30 @@
-const assert = requiire('assert.js');
-const calculateNumber = require("./0-calcul.js");
+const assert = require('assert');
+const { it, describe } = require('mocha');
+const calculateNumber = require('./0-calcul');
 
-describe('test suite 1', calculateNumber() {
-  it('adds numbers' , calculateNumber() {
-    assert.equal(calculateNumber(1, 3), 4);
+describe('calculateNumber()', function() {
+  it('checking if numbers are rounded', function() {
+    const result = calculateNumber(2, 2);
+    assert.strictEqual(result, 4);
+  });
+
+  it('checking if numbers are rounded', function() {
+    const result = calculateNumber(2.2, 2);
+    assert.strictEqual(result, 4);
+  });
+
+  it('checking if numbers are rounded', function() {
+    const result = calculateNumber(2, 2.2);
+    assert.strictEqual(result, 4);
+  });
+
+  it('checking if numbers are rounded', function() {
+    const result = calculateNumber(2.6, 2);
+    assert.strictEqual(result, 5);
+  });
+
+  it('checking if numbers are rounded', function() {
+    const result = calculateNumber(1.5, 3.7);
+    assert.strictEqual(result, 6);
   });
 });
