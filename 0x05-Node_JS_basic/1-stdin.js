e44e545396f09111f88bbc6a76ae2961a3ac02c1
m00n-die <reginaldrock219@gmail.com>
2024-05-07 23:42:48 +0100

    Finally fixed?

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,12 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-
-process.stdin.on("end", function() {
-  process.stdout.write("This important software is now closing");
-});
-
-rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
-  console.log(`Your name is: ${name}`);
-  rl.close();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
