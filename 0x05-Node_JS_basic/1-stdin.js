00c0c23e590f44a06a0067f1637f2f8f14abc9b5
m00n-die <reginaldrock219@gmail.com>
2024-05-07 23:18:17 +0100

    Task 01

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,11 +2,10 @@ const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
 const rl = readline.createInterface({ input, output });
 
-if (!process.stdin.isTTY) {
-  process.on("exit", function() {
-    console.log("This important software is now closing");
-  });
-};
+
+process.on("exit", function() {
+  console.log("This important software is now closing");
+});
 
 rl.question("Welcome to Holberton School, what is your name?\n", (name) => {
   console.log(`Your name is: ${name}`);
