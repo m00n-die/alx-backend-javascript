79e851cdf5ede220c164bd99f3ffbedbf7d6fe4a
m00n-die <reginaldrock219@gmail.com>
2024-05-07 23:31:57 +0100

    Task 01

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,8 @@
-const readline = require('node:readline');
-const { stdin: input, stdout: output } = require('node:process');
-const rl = readline.createInterface({ input, output });
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 
 process.stdin.on("end", function() {
