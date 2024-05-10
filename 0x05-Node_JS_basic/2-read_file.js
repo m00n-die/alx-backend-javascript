5a47b4076dd7ad122401c09bb6957b36ed21d4a2
m00n-die <reginaldrock219@gmail.com>
2024-05-10 19:42:04 +0100

    task 02

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    // read file data and separate by new lines
+    let fileData = fs.readFileSync(path, 'utf8').toString().split('\n');
+    fileData = fileData.slice(1, fileData.length - 1);
+    console.log(`Number of students: ${fileData.length}`);
+
+    const obj = {};
+    fileData.forEach((element) => {
+      const student = element.split(',');
+
+      if (!obj[student[3]]) obj[student[3]] = [];
+      obj[student[3]].push(student[0]);
+    });
+
+    for (const i in obj) {
+      if (i) console.log(`Number of students in ${i}: ${obj[i].length}. List: ${obj[i].join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the fileDatabase');
+  }
+}
+
+module.exports = countStudents;
